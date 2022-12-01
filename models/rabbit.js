e1888907a501068f7ab8298ea1f39f71875aac17
Hruthik Reddy Vadde <112142362+hruthik7@users.noreply.github.com>
2022-11-30 22:50:04 -0600

    updated rabbit

--- models/rabbit.js
+++ models/rabbit.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose")
 const rabbitSchema = mongoose.Schema({
 rabbit_Name: String,
-rabbit_Price: Number,
+rabbit_Price:{type: Number, min:5000, max:40000},
 rabbit_gender: String
 })
 module.exports = mongoose.model("rabbit",
